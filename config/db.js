4413abaf516fbcec3cedc299fbc10e4f7fdadc52
Adam K <adam@kecskes.net>
2020-07-06 14:21:19 -0500

    finished server side other than connecting to client

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `Mongo DB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
+// Initialization
 dotenv.config({ path: './config/config.env' });
-
+connectDB();
+const transactions = require('./routes/transactions');
 const app = express();
 
-app.get('/', (req, res) => res.send('hello'));
+// Middleware
+app.use(express.json());
 
+// Routing
+app.use('/api/v1/transactions', transactions);
+
+// Port handling
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
